1663b87e9bee29dd9cdcd7f02317c9f5166cf7a9
kdzro <ernestogithub@gmail.com>
2025-08-26 11:03:29 -0300

    update: added some configurations for java, c++ and c# development and some new plugins

---
+++ .config/nvim/lua/ernesto/plugins/barbecue.lua
@@ -0,0 +1,10 @@
+return {
+	"utilyre/barbecue.nvim",
+	dependencies = {
+		{ "SmiteshP/nvim-navic", dependencies = { "neovim/nvim-lspconfig" } },
+		"nvim-tree/nvim-web-devicons",
+	},
+	name = "barbecue",
+	version = "*",
+	opts = {},
+}
